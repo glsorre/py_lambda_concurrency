50569824779cd4fcdade90035320ead39bb50892
Giuseppe Sorrentino <uhpnou@gmail.com>
2020-03-23 09:32:17 +0100

    refactored with asyncio

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
 from math import factorial
 from multiprocessing import Process
 from queue import Queue
-from resource import getrusage, RUSAGE_SELF
+from random import randint
 from timeit import timeit
 from urllib import request
 
@@ -16,47 +16,47 @@ def cpu_bounded_func(n):
 def io_bounded_func(url):
     return request.urlopen(url)
 
-def sequential_io_handler():
+def sequential_io_handler(event={}, lambda_context={}):
     for url in range(10):
         io_bounded_func('https://docs.python.org/3/')
 
-def sequential_cpu_handler():
+def sequential_cpu_handler(event={}, lambda_context={}):
     for i in range(10000):
         cpu_bounded_func(5000)
 
-def thread_io_handler():
+def thread_io_handler(event={}, lambda_context={}):
     executor = ThreadPoolExecutor(max_workers=5)
     executor.map(io_bounded_func, ['https://docs.python.org/3/' for x in range(10)])
     executor.shutdown()
 
-def thread_cpu_handler():
+def thread_cpu_handler(event={}, lambda_context={}):
     executor = ThreadPoolExecutor(max_workers=5)
     executor.map(cpu_bounded_func, [5000 for x in range(10000)])
     executor.shutdown()
 
-def process_io_handler():
+def process_io_handler(event={}, lambda_context={}):
     executor = ProcessPoolExecutor(max_workers=5)
     executor.map(io_bounded_func, ['https://docs.python.org/3/' for x in range(10)])
     executor.shutdown()
 
-def process_cpu_handler():
+def process_cpu_handler(event={}, lambda_context={}):
     executor = ProcessPoolExecutor(max_workers=5)
     executor.map(cpu_bounded_func, [5000 for x in range(10000)])
     executor.shutdown()
 
-def process_cpu_handler_no_executor():
-    data = [5000 for x in range(10000)]
+def process_cpu_handler_no_executor(event={}, lambda_context={}):
+    data = [randint(100, 5000) for x in range(10000)]
     pool = Pool(5)
     pool.map(cpu_bounded_func, data)
 
 if __name__ == "__main__":
     count = 1
-    # print("*** IO Task ***")
-    # print("Sequential", timeit(sequential_io_handler, number=count), getrusage(RUSAGE_SELF).ru_maxrss)
-    # print("Threaded", timeit(thread_io_handler, number=count), getrusage(RUSAGE_SELF).ru_maxrss)
-    # print("Multiprocess", timeit(process_io_handler, number=count), getrusage(RUSAGE_SELF).ru_maxrss)
+    print("*** IO Task ***")
+    # print("Sequential", timeit(sequential_io_handler, number=count))
+    # print("Threaded", timeit(thread_io_handler, number=count))
+    # print("Multiprocess", timeit(process_io_handler, number=count))
     # print("*** CPU Task ***")
-    # print("Sequential", timeit(sequential_cpu_handler, number=count), getrusage(RUSAGE_SELF).ru_maxrss)
-    # print("Threaded", timeit(thread_cpu_handler, number=count), getrusage(RUSAGE_SELF).ru_maxrss)
-    # print("Multiprocess", timeit(process_cpu_handler, number=count), getrusage(RUSAGE_SELF).ru_maxrss)
-    print("Multiprocess no executor", timeit(process_cpu_handler_no_executor, number=count), getrusage(RUSAGE_SELF).ru_maxrss)
+    #print("Sequential", timeit(sequential_cpu_handler, number=count))
+    #print("Threaded", timeit(thread_cpu_handler, number=count))
+    #print("Multiprocess", timeit(process_cpu_handler, number=count))
+    print("Multiprocess no executor", timeit(process_cpu_handler_no_executor, number=count))
--- pool.py
+++ pool.py
@@ -1,41 +1,78 @@
 import logging
+import asyncio
 
+from concurrent.futures import ThreadPoolExecutor
 from multiprocessing import Process
 from threading import current_thread, Thread
 from queue import Queue
 
 logging.basicConfig(level=logging.INFO)
 
-def _run(q):
-    while True:
-        logging.info(f"{current_thread().name} {q.qsize()}")
-        p = q.get()
-        if p is None:
-            break
-        p.start()
-        p.join()
-        q.task_done()
+async def _run(p, counter):
+    # while True:
+    logging.info(f"{counter} {p.name}")
+    #     # logging.info(f"{current_thread().name}")
+    #     p = q.get()
+    #     if p is None:
+    #         break
+    p.start()
+    p.join()
+    p.close()
+    while p.is_alive():
+        await asyncio.sleep(0.01)
+    logging.info(f"CLOSED")
+    
+    #q.task_done()
+    
+async def _safe_run(p, counter, sem):
+    logging.info(sem)
+    async with sem:
+        logging.info("INNNNNNNNNNNNNNNNNNNNNNNNNN")
+        return await _run(p, counter)
+
+async def _main_safe_run(q, sem):
+    logging.info(sem)
+    tasks = [
+        asyncio.ensure_future(_safe_run(q[i], i, sem))
+        for i
+        in range(len(q))
+    ]
+    await asyncio.gather(*tasks)      
 
 class Pool:
     def __init__(self, max_workers):
         self.max_workers = max_workers
         self.threads = []
-        self.queue = Queue()
+        self.queue = []
+        self.sem = asyncio.Semaphore(max_workers)
 
     def map(self, func, args):
-        for i in range(self.max_workers):
-            t = Thread(target=_run, args=(self.queue, ))
-            t.start()
-            self.threads.append(t)
+        counter = len(args)
 
         for a in args:
-            self.queue.put(Process(target=func, args=(a, )))
+            self.queue.append(Process(target=func, args=(a, )))
 
-        self.queue.join()
+        # for i in range(self.max_workers):
+        #     t = Thread(target=_run, args=(self.queue, ))
+        #     t.start()
+        #     logging.info(f"{t.name} STARTED")
+        #     self.threads.append(t)
+        
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(_main_safe_run(self.queue, self.sem))
+        loop.run_until_complete(loop.shutdown_asyncgens())
+        loop.close()
 
-        for i in range(self.max_workers):
-            self.queue.put(None)
+        # executor = ThreadPoolExecutor(self.max_workers)
+        # prova = executor.map(_run, self.queue)
 
-        for t in self.threads:
-            logging.info(f"{t.name} JOINED")
-            t.join()
+        #self.queue.join()
+
+        # for i in range(self.max_workers):
+        #     self.queue.put(None)
+
+        # executor.shutdown()
+
+        # for t in self.threads:
+        #     logging.info(f"{t.name} JOINED")
+        #     t.join()
