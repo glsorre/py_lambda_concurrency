088af4b97e4b0c5b11ed8f53faaf1392149f42ac
Giuseppe Lucio Sorrentino <gsorrentino@hotels.com>
2020-03-23 14:43:43 +0100

    pool_async cleaned

--- pool_async.py
+++ pool_async.py
@@ -1,19 +1,13 @@
 import logging
 import asyncio
 
-from concurrent.futures import ThreadPoolExecutor
-from multiprocessing import Process
-from threading import current_thread, Thread
-from queue import Queue
-from random import randint
+from multiprocessing import Process, Queue
 
 logging.basicConfig(level=logging.INFO)
 
 async def _run(p, counter):
-    logging.info(f"{counter} {p.name}")
-    await p.wait()
-    logging.info(f"{counter} CLOSED")
-    
+    return await p.wait()
+
 async def _safe_run(p, counter, sem):
     async with sem:
         return await _run(p, counter)
@@ -24,7 +18,7 @@ async def _main_safe_run(q, sem):
         for i
         in range(len(q))
     ]
-    await asyncio.gather(*tasks)
+    return await asyncio.gather(*tasks)
 
 class AsyncProcess(Process):
     def __init__(self, target, args):
@@ -49,26 +43,10 @@ class Pool:
         for a in args:
             self.queue.append(AsyncProcess(target=func, args=(a, )))
 
-        # for i in range(self.max_workers):
-        #     t = Thread(target=_run, args=(self.queue, ))
-        #     t.start()
-        #     logging.info(f"{t.name} STARTED")
-        #     self.threads.append(t)
         loop = asyncio.get_event_loop()
-        loop.run_until_complete(_main_safe_run(self.queue, self.sem))
+        prova = loop.run_until_complete(_main_safe_run(self.queue, self.sem))
         loop.run_until_complete(loop.shutdown_asyncgens())
         loop.close()
 
-        # executor = ThreadPoolExecutor(self.max_workers)
-        # prova = executor.map(_run, self.queue)
-
-        #self.queue.join()
-
-        # for i in range(self.max_workers):
-        #     self.queue.put(None)
-
-        # executor.shutdown()
-
-        # for t in self.threads:
-        #     logging.info(f"{t.name} JOINED")
-        #     t.join()
+        for test in prova:
+            print(test)
