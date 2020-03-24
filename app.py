d1023790d7963ea737fdb7f48daf81bd12b9831b
Giuseppe Lucio Sorrentino <gsorrentino@hotels.com>
2020-03-24 17:08:26 +0100

    created global parameters

--- app.py
+++ app.py
@@ -5,6 +5,12 @@ from urllib import request
 
 from pool_thread import Pool
 
+CPU_TOT_NUMBER = 20000
+CPU_FUNC_NUMBER = 5000
+CPU_POOL_SIZE = 24
+IO_TOT_NUMBER = 10
+IO_POOL_SIZE = 12
+
 def cpu_bounded_func(n):
     return factorial(n)
 
@@ -17,36 +23,36 @@ def io_bounded_func(url):
     return request.urlopen(url)
 
 def sequential_io_handler(event={}, lambda_context={}):
-    for url in range(10):
+    for url in range(IO_TOT_NUMBER):
         io_bounded_func('https://docs.python.org/3/')
 
 def sequential_cpu_handler(event={}, lambda_context={}):
-    for i in range(5000):
-        cpu_bounded_func(5000)
+    for i in range(CPU_TOT_NUMBER):
+        cpu_bounded_func(CPU_FUNC_NUMBER)
 
 def thread_io_handler(event={}, lambda_context={}):
-    executor = ThreadPoolExecutor(max_workers=5)
-    executor.map(io_bounded_func, ['https://docs.python.org/3/' for x in range(10)])
+    executor = ThreadPoolExecutor(max_workers=IO_POOL_SIZE)
+    executor.map(io_bounded_func, ['https://docs.python.org/3/' for x in range(IO_TOT_NUMBER)])
     executor.shutdown()
 
 def thread_cpu_handler(event={}, lambda_context={}):
     executor = ThreadPoolExecutor(max_workers=5)
-    executor.map(cpu_bounded_func, [5000 for x in range(5000)])
+    executor.map(cpu_bounded_func, [CPU_TOT_NUMBER for x in range(CPU_FUNC_NUMBER)])
     executor.shutdown()
 
 def process_io_handler(event={}, lambda_context={}):
-    executor = ProcessPoolExecutor(max_workers=5)
-    executor.map(io_bounded_func, ['https://docs.python.org/3/' for x in range(10)])
+    executor = ProcessPoolExecutor(max_workers=IO_POOL_SIZE)
+    executor.map(io_bounded_func, ['https://docs.python.org/3/' for x in range(IO_TOT_NUMBER)])
     executor.shutdown()
 
 def process_cpu_handler(event={}, lambda_context={}):
-    executor = ProcessPoolExecutor(max_workers=5)
-    executor.map(cpu_bounded_func, [5000 for x in range(5000)])
+    executor = ProcessPoolExecutor(max_workers=CPU_POOL_SIZE)
+    executor.map(cpu_bounded_func, [CPU_FUNC_NUMBER for x in range(CPU_TOT_NUMBER)])
     executor.shutdown()
 
 def process_cpu_handler_no_executor(event={}, lambda_context={}):
-    data = [5000 for x in range(5000)]
-    pool = Pool(5)
+    data = [CPU_FUNC_NUMBER for x in range(CPU_TOT_NUMBER)]
+    pool = Pool(CPU_POOL_SIZE)
     pool.map(cpu_bounded_func, data)
 
 def lambda_handler(event, lambda_context):
